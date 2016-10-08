70045beca272b0f7da1986b58d73fcbb9bcc86ef
reinamykt <reina.t.m.123@gmail.com>
2016-10-08 01:48:49 +0000

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  
+ before_action :set_params, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+ 
   def show # 追加
-   @user = User.find(params[:id])
   end
   
   def new
@@ -19,14 +20,14 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(parame[:id])
   end
   
   def update
-    if @user.update(user_params)
-      # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: '基本情報を編集しました'
+  　 if @user.update(user_params)
+      flash[:success] = "Updated your Plofile"
+      redirect_to @user
     else
-      # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
     end
   end
@@ -34,7 +35,15 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :profile, :area, :email, :password,
                                  :password_confirmation)
   end
-end
+  
+  def set_params
+    @user = User.find(params[:id])
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928153241) do
+ActiveRecord::Schema.define(version: 20161007114103) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160928153241) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "profile"
+    t.string   "area"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
