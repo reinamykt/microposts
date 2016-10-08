26b7e3c985aab0a90d7de79c1b9ed6a588f5b3e0
reinamykt <reina.t.m.123@gmail.com>
2016-10-08 09:01:51 +0000

    add user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
- before_action :set_params, only: [:show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
- 
-  def show # 追加
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   def new
@@ -13,37 +13,35 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user # ここを修正
+      redirect_to user_path(@user) # ここを修正
     else
       render 'new'
     end
   end
   
   def edit
-    @user = User.find(parame[:id])
+    @user = User.find(params[:id])
   end
-  
+
   def update
-  　 if @user.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
       flash[:success] = "Updated your Plofile"
       redirect_to @user
     else
       render 'edit'
     end
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :profile, :area, :email, :password,
-                                 :password_confirmation)
-  end
-  
-  def set_params
-    @user = User.find(params[:id])
+                                   :password_confirmation)
   end
 
   def correct_user
     @user = User.find(params[:id])
     redirect_to root_path if @user != current_user
   end
+end
